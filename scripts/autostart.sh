caf15d9a0da780c372b1b5eb8fb5ecabba16fb39
Lordsofzzzz <adityakale2254@gmail.com>
2025-10-22 20:43:43 +0530

    new version

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 set -x
 
-#set wallpaper
-wallpaper=$(find {WALLPAPER LOCATION}-type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+killall waybar #killing waybar
 
-# Set wallpaper with swww
-swww img $wallpaper
 
-# Generate colors with pywal
-wal -i $wallpaper
+# Check if swww-daemon is running, and start it if it is not
+if ! pgrep -x "swww-daemon" > /dev/null
+then
+    swww-daemon &
+fi
+
+# Set a random wallpaper
+wallpaper=$(find /home/lord/Downloads -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1)
+
+# Set wallpaper with swww, but only if a wallpaper was found
+if [ -n "$wallpaper" ]; then
+    swww img "$wallpaper"
+
+    # Generate colors with pywal
+    wal -i "$wallpaper"
+fi
 
 # Start Waybar
 waybar &
+
