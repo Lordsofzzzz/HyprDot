d0a743881dd991149f92dffa18e956a7645e09c1
lord <lord@lord>
2026-06-02 09:47:39 +0530

    chore(hypr): spring animation for windows, scale auto revert back to 1.0

--- hypr/curves.lua
+++ hypr/curves.lua
@@ -4,3 +4,4 @@ hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}
 hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}    } })
 hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1} } })
 hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}  } })
+hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
